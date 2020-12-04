f6a44629e6ae7580494a6b284d9a8b7e8a7fb71d
Daniel Skeen <dcskeen@umich.edu>
2020-12-03 20:35:20 -0500

    Added mod_p and mod_q functions and fixed implementation of in_set_Zrp

--- src/Verifier/helpers.py
+++ src/Verifier/helpers.py
@@ -1,14 +1,18 @@
-from .parameters import Parameters
+from parameters import Parameters
 
 def in_set_Zq(num: int, param: Parameters) -> bool:
     q = param.get_small_prime_q()
 
-
-    return -1 < num < q
+    return 0 <= num < q
 
 def in_set_Zrp(num: int, param: Parameters) -> bool:
     q = param.get_small_prime_q()
-
     p = param.get_large_prime_p()
+    
+    return 0 <= num < p and int(pow(num, q, p)) == 1
 
-    return -1 < num < q and pow(num, q) % p == 1
+def mod_q(num: int, param: Parameters) -> int:
+    return int(num) % param.get_small_prime_q
+
+def mod_p(num: int, param: Parameters) -> int:
+    return int(num) % param.get_large_prime_p
