e2636338ee1daa66cc518caae62a002630729dfb
VM <kevinkz@umich.edu>
2020-12-06 12:33:06 -0500

    update

--- http/insta485/api/__init__.py
+++ http/insta485/api/__init__.py
@@ -1,8 +1,4 @@
 """Insta485 REST API."""
 
-from insta485.api.likes import get_likes
-from insta485.api.likes import delete_likes
 from insta485.api.likes import get_resource
-from insta485.api.likes import get_tenpost
-from insta485.api.likes import get_post
-from insta485.api.likes import get_comment
+
